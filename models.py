1a0011acbc7a4cae5e5bf496d21dd57a8aec61ef
philipperemy <premy@reactive.co.jp>
2017-05-16 14:12:41 +0900

    add m11 and m18

--- models.py
+++ models.py
@@ -45,7 +45,6 @@ def m5(num_classes=10):
     m.add(Activation('relu'))
     m.add(MaxPooling1D(pool_size=4, strides=None))
     m.add(Conv1D(128,
-                 input_shape=[AUDIO_LENGTH, 1],
                  kernel_size=3,
                  strides=1,
                  padding='same',
@@ -55,7 +54,6 @@ def m5(num_classes=10):
     m.add(Activation('relu'))
     m.add(MaxPooling1D(pool_size=4, strides=None))
     m.add(Conv1D(256,
-                 input_shape=[AUDIO_LENGTH, 1],
                  kernel_size=3,
                  strides=1,
                  padding='same',
@@ -65,7 +63,6 @@ def m5(num_classes=10):
     m.add(Activation('relu'))
     m.add(MaxPooling1D(pool_size=4, strides=None))
     m.add(Conv1D(512,
-                 input_shape=[AUDIO_LENGTH, 1],
                  kernel_size=3,
                  strides=1,
                  padding='same',
@@ -73,6 +70,128 @@ def m5(num_classes=10):
                  kernel_regularizer=regularizers.l2(l=0.0001)))
     m.add(BatchNormalization())
     m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+    m.add(Lambda(lambda x: K.mean(x, axis=1)))
+    m.add(Dense(num_classes, activation='softmax'))
+    return m
+
+
+def m11(num_classes=10):
+    m = Sequential()
+    m.add(Conv1D(64,
+                 input_shape=[AUDIO_LENGTH, 1],
+                 kernel_size=80,
+                 strides=4,
+                 padding='same',
+                 kernel_initializer='glorot_uniform',
+                 kernel_regularizer=regularizers.l2(l=0.0001)))
+    m.add(BatchNormalization())
+    m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+
+    for i in range(2):
+        m.add(Conv1D(64,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+
+    for i in range(2):
+        m.add(Conv1D(128,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+
+    for i in range(3):
+        m.add(Conv1D(256,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+
+    for i in range(2):
+        m.add(Conv1D(512,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+    m.add(Lambda(lambda x: K.mean(x, axis=1)))
+    m.add(Dense(num_classes, activation='softmax'))
+    return m
+
+
+def m18(num_classes=10):
+    m = Sequential()
+    m.add(Conv1D(64,
+                 input_shape=[AUDIO_LENGTH, 1],
+                 kernel_size=80,
+                 strides=4,
+                 padding='same',
+                 kernel_initializer='glorot_uniform',
+                 kernel_regularizer=regularizers.l2(l=0.0001)))
+    m.add(BatchNormalization())
+    m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+
+    for i in range(4):
+        m.add(Conv1D(64,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+
+    for i in range(4):
+        m.add(Conv1D(128,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+
+    for i in range(4):
+        m.add(Conv1D(256,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+
+    for i in range(4):
+        m.add(Conv1D(512,
+                     kernel_size=3,
+                     strides=1,
+                     padding='same',
+                     kernel_initializer='glorot_uniform',
+                     kernel_regularizer=regularizers.l2(l=0.0001)))
+        m.add(BatchNormalization())
+        m.add(Activation('relu'))
+
     m.add(MaxPooling1D(pool_size=4, strides=None))
     m.add(Lambda(lambda x: K.mean(x, axis=1)))
     m.add(Dense(num_classes, activation='softmax'))
