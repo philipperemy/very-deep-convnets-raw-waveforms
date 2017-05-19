bf6ac1a97299e445beead03245c43232258e7b5a
philipperemy <premy@reactive.co.jp>
2017-05-19 15:38:36 +0900

    update

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 import keras.backend as K
+from keras import regularizers
 from keras.layers import Lambda
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+from keras.layers.core import Activation, Dense
+from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
 
 from constants import *
-from model_layers import *
 
 
 def m3(num_classes=10):
@@ -137,6 +140,33 @@ def m11(num_classes=10):
         m.add(Activation('relu'))
 
     m.add(Lambda(lambda x: K.mean(x, axis=1)))
+    m.add(Dense(num_classes, activation='softmax'))
+    return m
+
+
+def m_rec(num_classes=10):
+    from keras.layers.recurrent import LSTM
+    print('Using Model LSTM 1')
+    m = Sequential()
+    m.add(Conv1D(64,
+                 input_shape=[AUDIO_LENGTH, 1],
+                 kernel_size=80,
+                 strides=4,
+                 padding='same',
+                 kernel_initializer='glorot_uniform',
+                 kernel_regularizer=regularizers.l2(l=0.0001)))
+    m.add(BatchNormalization())
+    m.add(Activation('relu'))
+    m.add(MaxPooling1D(pool_size=4, strides=None))
+    m.add(LSTM(32,
+               kernel_regularizer=regularizers.l2(l=0.0001),
+               return_sequences=True,
+               dropout=0.2))
+    m.add(LSTM(32,
+               kernel_regularizer=regularizers.l2(l=0.0001),
+               return_sequences=False,
+               dropout=0.2))
+    m.add(Dense(32))
     m.add(Dense(num_classes, activation='softmax'))
     return m
 
